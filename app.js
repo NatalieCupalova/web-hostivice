f5d299e11579f5df9bb2464c5e2af2902ca40dd5
ErikCupal <erik@cup.al>
2017-03-16 23:39:38 +0100

    possible fix

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const fs = require('fs')
 const http = require('http')
 const express = require('express')
 const createHandler = require('github-webhook-handler')
+var GithubWebHook = require('express-github-webhook');
 const bodyParser = require('body-parser')
 
 const key = fs.readFileSync('../nc-secret', 'utf8')
-const handler = createHandler({ path: '/github-webhook', secret: key })
+// const handler = createHandler({ path: '/github-webhook', secret: key })
+const handler = GithubWebHook({ path: '/github-webhook', secret: key })
 
 const app = express()
 // const server = http.createServer((req, res) => {
@@ -35,17 +37,14 @@ app.get('/', (req, res) => {
 // Configure Github Webhook
 
 app.use(bodyParser.json())
-
-app.post('/github-webhook', (req, res) => {
-
-  handler(req, res, (err) => {
-    res.statusCode = 404
-    res.end('no such location')
-  })
-})
+app.use(handler)
 
 handler.on('push', (event) => {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref)
+})
+
+handler.on('*', (event) => {
+  console.log('Heyoa')
 })
